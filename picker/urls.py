81df3de0f5bf6aaeb55fdda49f7048f358324cb9
Zahra Shamsi <shamsi@soc1.ir>
2023-06-08 22:42:35 +0200

    Serve static files by the application

--- picker/urls.py
+++ picker/urls.py
@@ -1,5 +1,8 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 
 urlpatterns = [
     path('storage/', include('storage.urls'))
+
 ]
+urlpatterns += staticfiles_urlpatterns()
